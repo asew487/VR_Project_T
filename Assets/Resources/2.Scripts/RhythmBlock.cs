0e1f15b5af6b9ba641cb6e77535e06cfb60b4726
MinJae <minjae8872@naver.com>
2025-08-29 23:09:18 +0900

    .

---
+++ Assets/Resources/2.Scripts/RhythmBlock.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class RhythmBlock : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Resources/2.Scripts/RhythmPlayer.cs
+++ Assets/Resources/2.Scripts/RhythmPlayer.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class RhythmPlayer : MonoBehaviour
 {
     [SerializeField] Rhythm _rhythm;
+    [SerializeField] RhythmBlock[] _rhythmBlocks;
     private int _rhythmIndex = 0;
     private float _beatTime = 0;
     private float _beatTimer = 0;
@@ -11,52 +12,36 @@ public class RhythmPlayer : MonoBehaviour
 
     private void Start()
     {
-        //StartCoroutine(PlayRhythm());
+        StartCoroutine(PlayRhythm());
     }
 
-    private void Update()
+    private void ActiveBlock(RhythmNote rhythmNote)
     {
-        if(_rhythmIndex < _rhythm.RhythmNotes.Count)
+        for(int i = 0; i < _rhythmBlocks.Length; i++)
         {
-            if(_isPlaying == false)
+            
+            if(_rhythmBlocks[i].gameObject.activeSelf == false)
             {
-                Debug.Log("딴");
-                float beatDuration = 60f / _rhythm.Bpm;
-                float bps = beatDuration * _rhythm.RhythmNotes[_rhythmIndex].Beats;
-                _beatTime = bps;
-                _isPlaying = true;
-            }
 
-            _beatTimer += Time.deltaTime;
-
-            if (_beatTimer > _beatTime)
-            {
-                _rhythmIndex++;
-                _beatTimer = 0;
-                _isPlaying = false;
             }
         }
-
-        
     }
 
     IEnumerator PlayRhythm()
     {
-        float beatDuration = 60f / _rhythm.Bpm; // 1박 시간(초)
-        float nextTime = Time.time;             // 첫 박 기준
-
-        while (_rhythmIndex < _rhythm.RhythmNotes.Count)
+        while(_rhythmIndex < _rhythm.RhythmNotes.Count)
         {
+            Debug.Log(Time.time);
+            float beatDuration = 60f / _rhythm.Bpm;
             float bps = beatDuration * _rhythm.RhythmNotes[_rhythmIndex].Beats;
+            float nextNote = Time.time + bps;
+            // 다음박 예비박 시작하기 - 스케일 bps 속도 동아 증가
 
-            Debug.Log($"{Time.time:F3}초 → 딴 (길이 {bps})");
-
-            // 다음 박 시간 예약
-            nextTime += bps;
-
-            // 다음 박 시간이 될 때까지 대기
-            while (Time.time < nextTime)
+            //yield return new WaitForSeconds(bps);
+            while(Time.time < nextNote)
+            {
                 yield return null;
+            }
 
             _rhythmIndex++;
         }
