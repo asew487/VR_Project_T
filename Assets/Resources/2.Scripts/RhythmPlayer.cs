ff89eb0f13418979efe15c8a08956416a70530c4
MinJae <minjae8872@naver.com>
2025-09-01 01:34:02 +0900

    .

--- Assets/Resources/2.Scripts/RhythmPlayer.cs
+++ Assets/Resources/2.Scripts/RhythmPlayer.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class RhythmPlayer : MonoBehaviour
 {
+    [SerializeField] GameObject _player;
     [SerializeField] Rhythm _rhythm;
     [SerializeField] RhythmBlock[] _rhythmBlocks;
     [SerializeField] float _nextNoteDuration;
@@ -16,25 +17,38 @@ public class RhythmPlayer : MonoBehaviour
         StartCoroutine(PlayRhythm());
     }
 
-    private void Update()
-    {
-        //if(Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    StartCoroutine(PlayRhythm());
-        //}
-    }
-
     private void ActiveBlock(float bps)
     {
-        for(int i = 0; i < _rhythmBlocks.Length; i++)
-        {
-            if(_rhythmBlocks[i].gameObject.activeSelf == false)
-            {
-                _rhythmBlocks[i].gameObject.SetActive(true);
-                _rhythmBlocks[i].Init(bps);
-                break;
-            }
-        }
+        //for(int i = 0; i < _rhythmBlocks.Length; i++)
+        //{
+        //    if(_rhythmBlocks[i].gameObject.activeSelf == false)
+        //    {
+        //        Vector3 targetPos = _rhythmBlocks[i].transform.position;
+        //        Vector3 playerPos = _player.transform.position;
+        //        targetPos.y = 0;
+        //        playerPos.y = 0;
+        //        Vector3 dir = targetPos - playerPos;
+        //        dir.Normalize();
+        //        Vector3 moveVec = _player.transform.position + dir * 0.5f;
+        //        moveVec.y = _rhythmBlocks[i].transform.position.y;
+        //        _rhythmBlocks[i].transform.position = moveVec;
+        //        _rhythmBlocks[i].gameObject.SetActive(true);
+        //        _rhythmBlocks[i].Init(bps);
+        //        break;
+        //    }
+        //}
+
+        Vector3 targetPos = _rhythmBlocks[_rhythmIndex].transform.position;
+        Vector3 playerPos = _player.transform.position;
+        targetPos.y = 0;
+        playerPos.y = 0;
+        Vector3 dir = targetPos - playerPos;
+        dir.Normalize();
+        Vector3 moveVec = _player.transform.position + dir * 0.5f;
+        moveVec.y = _rhythmBlocks[_rhythmIndex].transform.position.y;
+        _rhythmBlocks[_rhythmIndex].transform.position = moveVec;
+        _rhythmBlocks[_rhythmIndex].gameObject.SetActive(true);
+        _rhythmBlocks[_rhythmIndex].Init(bps);
     }
 
     IEnumerator PlayRhythm()
